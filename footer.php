9902ee314a837119a20b1bf389ce2bab2a3bcd97
ReginaBokova87 <rinka_bokova@mail.ru>
2017-05-31 23:16:09 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -6,12 +6,12 @@ if(empty($_COOKIE['session_key'])){
 	setcookie('session_key', $count2);
 	file_put_contents('files/counter2.txt',$count2);
 	}	
-	echo "<p><b>Число посетителей: ".$_COOKIE['session_key']."<br>";
+	echo '<p><b>Число посетителей: ".$_COOKIE['session_key']."<br>';
 
 		$handle = file_get_contents('files/counter.txt');
 		$handle++;
 		file_put_contents('files/counter.txt',$handle);
-		echo "<p><b> Число посещений: $handle";
+		echo '<p><b> Число посещений: ',$handle,'</b></p>;
 	?>
           
 <p><b>© 2016-2017 <a href="index.php" title="HockeyShop.Ru">HockeyShop.Ru</a> | Копирование материалов разрешено с указанием источника <a href="index.php" title="HockeyShop.Ru">HockeyShop.Ru</b></a></p>
