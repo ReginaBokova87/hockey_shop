8535069f7c9be17b377a7b20e5a09488e4f0c7fa
ReginaBokova87 <rinka_bokova@mail.ru>
2017-05-31 23:07:20 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,22 +1,17 @@
 <div id="footer">
 			<?php
 if(empty($_COOKIE['session_key'])){
-	$count2 = file_get_contents("files/counter2.txt");
+	$count2 = file_get_contents('files/counter2.txt');
 	$count2++;
 	setcookie('session_key', $count2);
-	
-	$fp = @fopen("files/counter2.txt", "w");
-	fwrite($fp, $count2);
-	fclose($fp);
+	file_put_contents('files/counter2.txt',$count2);
 	}	
 	echo "<p><b>Число посетителей: ".$_COOKIE['session_key']."<br>";
 ?>
 <?php
-		$handle = file_get_contents("files/counter.txt");
+		$handle = file_get_contents('files/counter.txt');
 		$handle++;
-		$fp = @fopen("files/counter.txt","w");
-		fwrite($fp,$handle);
-		fclose($fp);
+		file_put_contents('files/counter2.txt',$handle);
 		echo "<p><b> Число посещений: $handle";
 	?>
           
