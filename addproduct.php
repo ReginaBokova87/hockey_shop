fdf2e1e2c3a59db696a56bc12ec9e628034ccb33
ReginaBokova87 <rinka_bokova@mail.ru>
2017-06-02 05:27:32 +0700

    Update addproduct.php

--- addproduct.php
+++ addproduct.php
@@ -1,17 +1,18 @@
-<?session_start();
-if (isset($_SESSION['USER']['LOGIN']) && isset($_SESSION['adminmode'])) 
-{
-$productarticle=htmlspecialchars($_POST['productarticle']);
-$productname=htmlspecialchars($_POST['productname']);
-$productprice=(float)$_POST['productprice'];
-$productimg=$_FILES['productimg']['name'];
-$productsrc=htmlspecialchars($_POST['productsrc']);
-copy($_FILES['productimg']['tmp_name'], 'top/'.$_FILES['productimg']['name']);
-file_put_contents('files/top_productarticles.txt',$productarticle."\n", FILE_APPEND);
-file_put_contents('files/top_productnames.txt',$productname."\n", FILE_APPEND);
-file_put_contents('files/top_productprices.txt',$productprice."\n", FILE_APPEND);
-file_put_contents('files/top_productimgs.txt',$productimg."\n", FILE_APPEND);
-file_put_contents('files/top_productsrcs.txt',$productsrc."\n", FILE_APPEND);
-echo '<script>location.href="index.php"</script>';
-}
-else header('Location:index.php');?>
+<?
+session_start();
+if (isset($_SESSION['USER']['LOGIN']) && isset($_SESSION['adminmode'])) {
+    $productarticle = htmlspecialchars($_POST['productarticle']);
+    $productname    = htmlspecialchars($_POST['productname']);
+    $productprice   = (float) $_POST['productprice'];
+    $productimg     = $_FILES['productimg']['name'];
+    $productsrc     = htmlspecialchars($_POST['productsrc']);
+    copy($_FILES['productimg']['tmp_name'], 'top/' . $_FILES['productimg']['name']);
+    file_put_contents('files/top_productarticles.txt', $productarticle . "\n", FILE_APPEND);
+    file_put_contents('files/top_productnames.txt', $productname . "\n", FILE_APPEND);
+    file_put_contents('files/top_productprices.txt', $productprice . "\n", FILE_APPEND);
+    file_put_contents('files/top_productimgs.txt', $productimg . "\n", FILE_APPEND);
+    file_put_contents('files/top_productsrcs.txt', $productsrc . "\n", FILE_APPEND);
+    echo '<script>location.href="index.php"</script>';
+} else
+    header('Location:index.php');
+?>
