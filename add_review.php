730ff457c8da48136be7b819a33aa928494de346
ReginaBokova87 <rinka_bokova@mail.ru>
2017-06-01 17:38:16 +0700

    Add files via upload

---
+++ add_review.php
@@ -0,0 +1,10 @@
+<?php $names=htmlspecialchars($_POST['name']);
+$reviews=htmlspecialchars($_POST['review']);
+$reviews_dates=date("d.m.y");
+$reviews_times=date("H:i:s");
+file_put_contents('files/review_name.txt',$names."\n", FILE_APPEND);
+file_put_contents('files/review_text.txt',$reviews."\n", FILE_APPEND);
+file_put_contents('files/review_date.txt',$reviews_dates."\n", FILE_APPEND);
+file_put_contents('files/review_time.txt',$reviews_times."\n", FILE_APPEND);
+echo '<script>location.href="reviews.php"</script>';
+?>
