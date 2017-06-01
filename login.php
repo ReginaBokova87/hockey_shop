dbeeaba7a01b85d117f063227cdf91cd4fcabd38
ReginaBokova87 <rinka_bokova@mail.ru>
2017-06-02 01:00:30 +0700

    Update login.php

--- login.php
+++ login.php
@@ -13,7 +13,7 @@ if ($mysqli->connect_errno) {
         if(isset($_POST['submit'])) {
             $login = $_POST['login'];
             $password = md5($_POST['password']);
-            $query = "SELECT * FROM users WHERE `login` = "' . $mysqli->real_escape_string($login). '" AND `password` = "' . $mysqli->real_escape_string($password). '" ";
+            $query = "SELECT * FROM users WHERE `login` = ' . $mysqli->real_escape_string($login). ' AND `password` = ' . $mysqli->real_escape_string($password). ' ";
                 $result = $mysqli->query($query);
 				while($user=$result->fetch_assoc()){
                     if($user){
