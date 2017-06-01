b15398de7f9cf637cfe1ea285430d680a56cbe1d
ReginaBokova87 <rinka_bokova@mail.ru>
2017-06-02 05:35:27 +0700

    Update delfrombasket.php

--- delfrombasket.php
+++ delfrombasket.php
@@ -1,25 +1,23 @@
-<?php session_start();
-$itemnumber='';
-for ($i=0; $i<$_SESSION['basketcounter']; $i++)
-{
-if(!empty($_GET['product'.$i])){ 
-	$itemnumber=$_GET['product'.$i]; 
-if ($itemnumber!='') 
-{ 
-$itemnumber=$i; 
-break; 
-} 
-} else { 
-continue; 
-} 
+<?php
+session_start();
+$itemnumber = '';
+for ($i = 0; $i < $_SESSION['basketcounter']; $i++) {
+    if (!empty($_GET['product' . $i])) {
+        $itemnumber = $_GET['product' . $i];
+        if ($itemnumber != '') {
+            $itemnumber = $i;
+            break;
+        }
+    } else {
+        continue;
+    }
 }
-for ($i=$itemnumber; $i<$_SESSION['basketcounter']; $i++)
-{
-	$next=$i+1;
-	$_SESSION['item'.$i]=$_SESSION['item'.$next];
-	$_SESSION['item_name'.$i]=$_SESSION['item_name'.$next];
-	$_SESSION['price'.$i]=$_SESSION['price'.$next];
-	$_SESSION['quantity'.$i]=$_SESSION['quantity'.$next];
+for ($i = $itemnumber; $i < $_SESSION['basketcounter']; $i++) {
+    $next                       = $i + 1;
+    $_SESSION['item' . $i]      = $_SESSION['item' . $next];
+    $_SESSION['item_name' . $i] = $_SESSION['item_name' . $next];
+    $_SESSION['price' . $i]     = $_SESSION['price' . $next];
+    $_SESSION['quantity' . $i]  = $_SESSION['quantity' . $next];
 }
 $_SESSION['basketcounter']--;
 echo '<script>location.href="', $_SERVER['HTTP_REFERER'], '"</script>';
