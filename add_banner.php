ffe98ee84899550e4ec955defb5df1e1b16c1827
ReginaBokova87 <rinka_bokova@mail.ru>
2017-06-02 01:08:58 +0700

    Update add_banner.php

--- add_banner.php
+++ add_banner.php
@@ -7,7 +7,7 @@ file_put_contents('files/bannersrcs.txt', '');
 $bannername=$_POST['bannername'];
 $bannerimg=$_FILES['bannerimg']['name'];
 $bannersrc=$_POST['bannersrc'];
-copy($_FILES['bannerimg']['tmp_name'], 'top/'.$_FILES['bannerimg']['name']);
+copy($_FILES['bannerimg']['tmp_name'], 'banner/'.$_FILES['bannerimg']['name']);
 file_put_contents('files/bannernames.txt',$bannername."\n", FILE_APPEND);
 file_put_contents('files/bannerimgs.txt',$bannerimg."\n", FILE_APPEND);
 file_put_contents('files/bannersrcs.txt',$bannersrc."\n", FILE_APPEND);
