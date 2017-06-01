5f64dc8010f89cc993a34ac4c7c9921346d2ba04
ReginaBokova87 <rinka_bokova@mail.ru>
2017-06-02 05:38:03 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,14 +1,11 @@
 <div id="footer">
-			<?php
-		$handle = file_get_contents("counter.txt");
-		$handle++;
-		$fp = @fopen("counter.txt","w");
-		fwrite($fp,$handle);
-		fclose($fp);
-		echo "<p><b>Число посещений: $handle</b></p>";
-	?>
-	
-	
-          
-<p><b>© 2016-2017 <a href="index.php" title="HockeyShop.Ru">HockeyShop.Ru</a> | Копирование материалов разрешено с указанием источника <a href="index.php" title="HockeyShop.Ru">HockeyShop.Ru</b></a></p>
-</div>	
+   <?php
+      $handle = file_get_contents("counter.txt");
+      $handle++;
+      $fp = @fopen("counter.txt","w");
+      fwrite($fp,$handle);
+      fclose($fp);
+      echo "<p><b>Число посещений: $handle</b></p>";
+      ?>
+   <p><b>© 2016-2017 <a href="index.php" title="HockeyShop.Ru">HockeyShop.Ru</a> | Копирование материалов разрешено с указанием источника <a href="index.php" title="HockeyShop.Ru">HockeyShop.Ru</b></a></p>
+</div>
